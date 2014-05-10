f03b405f5438888117c6cf423f509aa6b0cf3040
María Guillermina Véscovo <guilleves@gmail.com>
2014-05-10 18:09:49 -0300

    Update asdasd.py

--- asdasd.py
+++ asdasd.py
@@ -1,8 +1,11 @@
 import random
+coef = pow(2, 30)-1
 
-n = random.randint(0, 90000)
+n = random.randint(0, coef)
+n = bin(n)[:2]
 
 while (len(str(n)) != 30):
-    n= random.randint(0, 90000)
+    n= random.randint(0, coef)
+    n = bin(n)[:2]
     
-print n
+print (int(n))
