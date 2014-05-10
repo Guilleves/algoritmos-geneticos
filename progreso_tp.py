c15a6e93c679b6c93f62f7a16a220deb711ce5b4
María Guillermina Véscovo <guilleves@gmail.com>
2014-05-10 15:22:49 -0300

    Update progreso_tp.py

--- progreso_tp.py
+++ progreso_tp.py
@@ -11,6 +11,7 @@ mutationProb = 0.05
 #
 #bloque de importacion de modulos
 import random
+import pprint
 #
 #
 #bloque de definicion de funciones
@@ -27,6 +28,7 @@ def promedio(valor):
 
 def ruleta(fitness):
 	porcentaje = (fitness*100)
+	return porcentaje
 
 def crossover(cromosoma1, cromosoma2, corte):
 	hijo = genList[cromosma1][:corte] + genList[cromosoma2][corte:]
@@ -34,11 +36,14 @@ def crossover(cromosoma1, cromosoma2, corte):
 
 #bloque del programa principal
 
+resultados = [] #historico de los resultados
 cromosomaList = [] #lista de los valores de c/cromosoma (int)
 genList = [] #lista de los genes de c/cromosoma (binario)
 objectiveList = [] #resultados de la funcion objetivo evaluada en los cromosomas enteros
 fitnessList = [] #resultados de la funcion fitness para c/cromosoma
 ruletaList = []
+matrixList = []
+porcentajeList = [] #guardo los porcentajes del fitness de c/cromosoma
 
 sumaList = []
 promedioList = []
@@ -48,14 +53,16 @@ indice = 0
 
 for i in xrange(20):
 
-	cromosomaList = [] #tambien puedo usar del list[:], da igual
+	#tambien puedo usar del list[:], da igual
 	genList = []
 	objectiveList = []
 	fitnessList = []
 	ruletaList = [] 
+	cromosomaListNew = []
+	porcentajeList = []
 	# zip() creo pares a partir de elementos de una lista
 	totalObjetivo = 0
-	indice = 0
+	indice = 0 #para guardar la posicion del cromosoma en la lista
 
 
 	for j in xrange(10):
@@ -75,17 +82,19 @@ for i in xrange(20):
 		fitnessList.append(resultadoFitness)
 		porcentaje = ruleta(resultadoFitness)#le asigno un valor porcentual en la ruleta
 		porcentajeList.append(porcentaje)
-			
-			for k in xrange(porcentaje):#voy cargando valores en un array de 100
-				ruletaList.append(indice)
-		indice++ #cuando terminan las 10 iteraciones la ruleta queda cargada
+		porcentaje = int(porcentaje)	
 
-	for l in xrange(5):
+			for k in xrange(porcentaje): #voy del ultimo valor hasta el porcentaje de este cromosoma
+				ruletaList.append(cromosomaList[indice]) #agrego el mismo cromosoma hasta que termine el for
+			
+			indice++#cuando terminan las 10 iteraciones la ruleta queda cargada
+
+	for l in xrange(5):#hago 5 loops porque son 5 pares
 		azarRuleta1 = random.randint(0, 99)
-		cromosomaAzar1 = cromosomaBin[azarRuleta1] #elijo el primer valor del par
+		cromosomaAzar1 = ruletaList[azarRuleta1] #elijo el primer valor del par
 
 		azarRuleta2 = random.randint(0, 99)
-		cromosomaAzar2 = cromosomaBin[azarRuleta2] #elijo el 2do valor del par
+		cromosomaAzar2 = ruletaList[azarRuleta2] #elijo el 2do valor del par
 
 		crossoverRandom = random.random() #tiro un random para ver el crossover
 
@@ -101,12 +110,13 @@ for i in xrange(20):
 			hijo1 = cromosomaAzar1
 			hijo2 = cromosomaAzar2
 
-GITHU      	mutationRandom2 = random.random
+		mutationRandom1 = random.random
+      	mutationRandom2 = random.random
 
 		if mutationRandom1 <= mutationProb:  #pruebo si muta y cambio el valor
-			genChange = random.randit(0, 29)
+			genChange = random.randint(0, 29)
 
-			if hijo1[genChange] = 0:
+			if (hijo1[genChange] == 0):
 				hijo1[genChange] = 1
 			else:
 				hijo1[genChange] = 0
@@ -114,9 +124,42 @@ GITHU      	mutationRandom2 = random.random
         if mutationRandom2 <= mutationProb:
 			genChange = random.randit(0, 29)
 
-			if hijo2[genChange] = 0:
+			if (hijo2[genChange] == 0):
 				hijo2[genChange] = 1
 			else:
 				hijo2[genChange] = 0
+
+		cromosomaListNew.append(hijo1) #guardo los cromosomas nuevos en otra lista
+		cromosomaListNew.append(hijo2)
+
+	cromosomaList = list(cromosomaListNew) #reemplazo la lista original con los hijos
+
+	promedioObjetivo = promedio(totalObjetivo)
+	maximoObjetivo =  max(objectiveList)
+	maximoFitness =  max(fitnessList)
+	sumaObjetivo = totalObjetivo
+	
+
+	matrix = [[0 for x in xrange(4)] for x in xrange(4)] #creo una matriz de resultados
+
+	matrix[0][0] = '    '	#columnas lindas
+	matrix[0][1] = 'F.Obj'
+	matrix[0][2] = 'Fitness'
+	matrix[1][0] = 'Suma'
+	matrix[2][0] = 'Prom'
+	matrix[3][0] = 'Max'
+
+	matrix[1][1] = totalObjetivo #completo los datos
+	matrix[1][2] = 1
+	matrix[2][1] = promedioObjetivo
+	matrix[2][2] = 0.10
+	matrix[3][1] = maximoObjetivo
+	matrix[3][2] = maximoFitness
+
+	matrixList.append(matrix)
+
+
+
+
 
 
